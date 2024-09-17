1e57f6012f15af80c1072f32bad768a40beeb928
ccmalcom <chasecmalcom@gmail.com>
2024-09-17 17:24:27 -0400

    initial work for sprite image

--- script.js
+++ script.js
@@ -111,3 +111,31 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 
 })
+
+document.addEventListener('mousemove', (e) => {
+    console.log(e);
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const anchor = document.getElementById('anchor');
+    const rect = anchor.getBoundingClientRect();
+    const anchorX = rect.left + rect.width / 2;
+    const anchorY = rect.top + rect.height / 2;
+
+    const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+    console.log(angleDeg);
+
+    const eyes = document.querySelectorAll('.eye');
+    eyes.forEach(eye => {
+        eye.style.transform = `rotate(${90 + angleDeg}deg)`;
+    });
+})
+
+const angle = (cx, cy, ex, ey) => {
+    const dy = ey - cy;
+    const dx = ex - cx;
+    let theta = Math.atan2(dy, dx);
+    theta *= 180 / Math.PI;
+    return theta;
+}
