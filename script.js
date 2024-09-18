d51210950cc588356223216b0270b4d8af7defbc
ccmalcom <chasecmalcom@gmail.com>
2024-09-17 21:52:50 -0400

    cleaned up css and using data-theme css tag

--- script.js
+++ script.js
@@ -2,17 +2,13 @@ const themeStylesheet = document.getElementById('themeStylesheet');
 const themeColor = document.getElementById('theme-color');
 let isDarkMode = localStorage.getItem('colorScheme') === 'dark' ||
     (localStorage.getItem('colorScheme') === null && window.matchMedia('(prefers-color-scheme: dark)').matches);
-themeStylesheet.href = isDarkMode ? '/dark-mode.css' : '/light-mode.css';
 themeColor.content = isDarkMode ? '#BB6B6B' : '#F7344F';
 
 // darkmode toggle 
-
 document.addEventListener('DOMContentLoaded', function () {
-
     const suns = document.querySelectorAll('.sun')
     const moons = document.querySelectorAll('.moon')
     const buttons = document.querySelectorAll('.modeContainer')
-
 
     suns.forEach(sun => sun.classList.toggle('visible', !isDarkMode));
     moons.forEach(moon => moon.classList.toggle('visible', isDarkMode));
@@ -21,12 +17,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
         button.addEventListener('click', () => {
             isDarkMode = !isDarkMode; // Toggle the boolean value
+            console.log(isDarkMode)
             suns.forEach(sun => sun.classList.toggle('visible'));
             moons.forEach(moon => moon.classList.toggle('visible'));
-            // darkModeStatus.textContent = isDarkMode ? 'enabled' : 'disabled';
-            themeStylesheet.href = isDarkMode ? '/dark-mode.css' : '/light-mode.css';
+
             themeColor.content = isDarkMode ? '#BB6B6B' : '#F7344F';
             localStorage.setItem('colorScheme', isDarkMode ? 'dark' : 'light');
+            document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
         });
     })
 
