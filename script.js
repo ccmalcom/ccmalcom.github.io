c07f98c2aabbdd40089bf99aa70afeb93660864b
ccmalcom <chasecmalcom@gmail.com>
2024-09-17 22:31:14 -0400

    cleaned up javascript

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const themeStylesheet = document.getElementById('themeStylesheet');
 const themeColor = document.getElementById('theme-color');
+
 let isDarkMode = localStorage.getItem('colorScheme') === 'dark' ||
     (localStorage.getItem('colorScheme') === null && window.matchMedia('(prefers-color-scheme: dark)').matches);
 themeColor.content = isDarkMode ? '#BB6B6B' : '#F7344F';
 
-// darkmode toggle 
 document.addEventListener('DOMContentLoaded', function () {
     const suns = document.querySelectorAll('.sun')
     const moons = document.querySelectorAll('.moon')
@@ -14,10 +13,8 @@ document.addEventListener('DOMContentLoaded', function () {
     moons.forEach(moon => moon.classList.toggle('visible', isDarkMode));
 
     buttons.forEach(button => {
-
         button.addEventListener('click', () => {
             isDarkMode = !isDarkMode; // Toggle the boolean value
-            console.log(isDarkMode)
             suns.forEach(sun => sun.classList.toggle('visible'));
             moons.forEach(moon => moon.classList.toggle('visible'));
 
